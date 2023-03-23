bc2ba37e1c6ed7fd4e1d95ac002861088b3e6c1d
alexander.brueck <alexander.brueck@credera.com>
2023-03-23 11:50:19 -0600

    Add nav bar

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -4,7 +4,39 @@
       <div
         class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
       >
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <a :href="url" class="flex h-full items-center text-xl">{{
+          company
+        }}</a>
+
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li class="h-full">
+              <a href="" class="flex h-full items-center py-2.5">Teams</a>
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5">Location</a>
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5"
+                >Life at Google</a
+              >
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5">How We Hire</a>
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5">Students</a>
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5">Jobs</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -15,7 +47,8 @@ export default {
   name: "MainNav",
   data() {
     return {
-      company: "Tesla Careers",
+      company: "Google Careers",
+      url: "https://careers.google.com",
     };
   },
 };
