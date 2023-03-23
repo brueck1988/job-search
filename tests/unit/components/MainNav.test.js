d3c3284abc5368404e864476ab952090a9c8bd03
alexander.brueck <alexander.brueck@credera.com>
2023-03-23 13:28:29 -0600

    Add test for nav bar

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -5,7 +5,23 @@ import MainNav from "@/components/MainNav.vue";
 describe("MainNav", () => {
   it("displays company name", () => {
     render(MainNav);
-    const companyName = screen.getByText("Tesla Careers");
+    const companyName = screen.getByText("Google Careers");
     expect(companyName).toBeInTheDocument();
+  });
+
+  it("displays menu items for navigation", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navigationMenuTexts = navigationMenuItems.map(
+      (item) => item.textContent
+    );
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Google",
+      "How We Hire",
+      "Students",
+      "Jobs",
+    ]);
   });
 });
