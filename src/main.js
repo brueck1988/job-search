9880cbbf48f745ee93b4b391f2cd8afa092e82d5
alexander.brueck <alexander.brueck@credera.com>
2023-03-24 16:25:54 -0600

    Make search icon available gloabally

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 import "@/index.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+library.add(faSearch);
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
+// The component method returns an app just like createApp(App), but
+// allows additional items to be added to app
+// .component("font-awesome-icon", FontAwesomeIcon) // this code makes the search icon available globally
