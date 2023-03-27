aafb53793afae09e8a425ba1496c739dc836c18a
alexander.brueck <alexander.brueck@credera.com>
2023-03-27 15:44:23 -0600

    Create theHeadline component

---
+++ src/components/TheHeadline.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>Build for Everyone</h1>
+    <h2>Find your next job at Google</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TheHeadline",
+	data() {
+		return {
+			sample: "Hello!"m
+		};
+	},
+	created() {
+		console.log("Hey, I've been created. My data exists. Look: ", this.sample)
+	}
+};
+</script>
