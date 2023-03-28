2911472e1689a415f48a22b189157b92d53b13cd
alexander.brueck <alexander.brueck@credera.com>
2023-03-28 14:11:20 -0600

    Mock example

---
+++ tests/unit/components/TheHeadline.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/vue";
+
+import TheHeadline from "@/components/TheHeadline.vue";
+
+describe("TheHeadline", () => {
+  // Example of how mock works
+  // describe("Vitest playground", () => {
+  //   it("tracks whether it has been called", () => {
+  //     const mockFunction = vi.fn(); //creates a mock function
+  //     mockFunction(1, 2); // Calls mock function
+  //     expect(mockFunction).toHaveBeenCalledWith(1, 2);
+  //   });
+  // });
+});
